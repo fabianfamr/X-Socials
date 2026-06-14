4abb27b88774c6613623a78ad165b1984d9dcc48
Fabian <fabianfamr@users.noreply.github.com>
2026-06-14 17:47:26 +0000

    Revert: restore colored startup/shutdown banners and logInfo

--- src/main/java/com/fabian/xsocials/XSocials.java
+++ src/main/java/com/fabian/xsocials/XSocials.java
@@ -12,6 +12,7 @@ import com.fabian.xsocials.utils.UpdateChecker;
 import com.fabian.xsocials.managers.StatsManager;
 import com.fabian.xsocials.metrics.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -128,10 +129,14 @@ public class XSocials extends JavaPlugin {
         // Initialize bStats Metrics
         setupMetrics();
 
-        getLogger().info(" ----------------------------------------------");
-        getLogger().info("   Enabled v" + getDescription().getVersion() + "! Socials are ready.");
-        getLogger().info("   Language: " + getConfig().getString("language", "EN").toUpperCase());
-        getLogger().info(" ----------------------------------------------");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                "&8[&bX-Socials&8] &7----------------------------------------------"));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                "&8[&bX-Socials&8]   &aEnabled v" + getDescription().getVersion() + "! Socials are ready."));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                "&8[&bX-Socials&8]   &7Language: &f" + getConfig().getString("language", "EN").toUpperCase()));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                "&8[&bX-Socials&8] &7----------------------------------------------"));
     }
 
     @Override
@@ -179,9 +184,12 @@ public class XSocials extends JavaPlugin {
     public void onDisable() {
         DebugLogger.debug("Init", "Plugin disabling...");
 
-        getLogger().info(" ----------------------------------------------");
-        getLogger().info("   Disabled v" + getDescription().getVersion() + "! Out.");
-        getLogger().info(" ----------------------------------------------");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                "&8[&bX-Socials&8] &7----------------------------------------------"));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                "&8[&bX-Socials&8]   &cDisabled v" + getDescription().getVersion() + "! Out."));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                "&8[&bX-Socials&8] &7----------------------------------------------"));
     }
 
     private void setupMetrics() {
